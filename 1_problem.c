3fb7368bb82411f771f3975320d3968af52ae62b
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-04-07 01:54:59 +0600

    init

---
+++ 1_problem.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+  
+  int main(){
+        
+        
+        char messeger[100];
+
+        
+        //scanf("%s", messeger);
+
+        gets(messeger);
+
+        printf("Hello, World!\n");
+        
+        //printf("%s",messeger);
+        puts(messeger);
+        
+    
+
+return 0;
+
+  }
+
